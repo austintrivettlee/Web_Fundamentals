21c9f2bfa2475e11fbb98ccbeab346616a6770c6
Austin Trivett Lee <austintrivettlee@gmail.com>
2023-07-14 19:16:40 -0500

    new website

--- Lectures/countPositives.js
+++ Lectures/countPositives.js
@@ -14,17 +14,34 @@ var numbers = [3, 4, -2, 7, 16, -8, 0];
 // L
 // K
 
-function countPositivesInArray(arrParam) {
-    var countPositives = 0;
 
-    for (var i = 0; i < arrParam.length; i++) {
-        if (arrParam[i] > 0) {
-            countPositives += 1
-        }
+
+var arr1 = [42, 3, 12, 8, 7, 89]
+
+
+function reverseArray(arr) {
+    var temp = 0;
+    for (var left = 0; left < arr.length / 2; left++) {
+        var right = arr.length - 1 - left;
+        temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
     }
-
-
-    console.log("there are " + countPositives + " positive values");
+    return arr
 }
 
-countPositivesInArray(numbers);
+reverseArray(arr1);
+console.log(arr1);
+
+
+
+
+
+
+
+
+
+const items = [1,2,3];
+
+console.log(items.reverse());
+
