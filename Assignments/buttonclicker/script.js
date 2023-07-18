5631f0168ba4fd24538189c27cd8a27eca72cbc4
Austin Trivett Lee <austintrivettlee@gmail.com>
2023-07-18 17:15:50 -0500

    completed a few different javascript website

---
+++ Assignments/buttonclicker/script.js
@@ -0,0 +1,12 @@
+function login() {
+        var butLogin = document.querySelector("#loginbut").innerText;
+        document.querySelector("#loginbut").innerText = "Logout";
+}
+
+function addButton(element) {
+    element.remove();
+}
+
+function addLike(element) {
+    alert("You have a Like!")
+}
---
+++ Assignments/likes/script.js
@@ -0,0 +1,18 @@
+function addLike() {
+    var likeCount = document.querySelector("#likes").innerText
+    likeCount = parseFloat(likeCount)
+    likeCount++;
+    document.querySelector("#likes").innerText = likeCount
+}
+function addLike2() {
+    var likeCount2 = document.querySelector("#likes2").innerText
+    likeCount2 = parseFloat(likeCount2)
+    likeCount2++;
+    document.querySelector("#likes2").innerText = likeCount2
+}
+function addLike3() {
+    var likeCount3 = document.querySelector("#likes3").innerText
+    likeCount3 = parseFloat(likeCount3)
+    likeCount3++;
+    document.querySelector("#likes3").innerText = likeCount3
+}
